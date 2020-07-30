8d4ac98e15ee5de63d69f81fda5bedf40097103c
Milos Popovic <milos_popovic@outlook.com>
2020-07-30 17:11:57 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,7 @@ end
 
 def add_backer(backer)
   @backers << backer
-  backer.backed_projects << self
+  backer.backed_projects << self #also adds the project to the backer's backed_projects array
 end
 
 
